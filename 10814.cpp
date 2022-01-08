21ed91f2e98faa9a2a947f8cd8051fc66d1bd40d
jinith <96716425+L-jin@users.noreply.github.com>
2022-01-08 23:58:10 +0900

    Create 10814.cpp

---
+++ 10814.cpp
@@ -0,0 +1,27 @@
+# include<iostream>
+# include<algorithm>
+using namespace std;
+
+ bool cmp(pair <int, string>a, pair <int, string>b) {
+	return a.first < b.first;
+}
+
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	pair<int, string>p[1000001];
+
+	for (int i = 0; i < n; i++) {
+		cin >> p[i].first >> p[i].second;
+	}
+
+	stable_sort(p, p+n,cmp);
+	for (int i = 0; i < n; i++) {
+		cout << p[i].first << ' ' << p[i].second << '\n';
+	}
+
+	return 0;
+}
