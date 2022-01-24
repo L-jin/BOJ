1b06386f28ec8e07f4e75914d4f69e887a208c89
jinith <96716425+L-jin@users.noreply.github.com>
2022-01-24 11:57:57 +0900

    Create 1932.cpp

---
+++ 1932.cpp
@@ -0,0 +1,37 @@
+//BOJ 1932 - 22.01.24
+
+
+# include<iostream>
+using namespace std;
+
+int arr[501][501];
+int dp[501][501];
+
+int main() {
+	int n;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++)
+			cin >> arr[i][j];
+	}
+
+
+	dp[1][1] = arr[1][1];
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			dp[i][j] = max(dp[i - 1][j - 1] + arr[i][j], dp[i - 1][j] + arr[i][j]);
+			cout << i << ' ' << j;
+		}
+	}
+
+	int ssum = 0;
+	for (int i = 1; i <= n; i++) {
+		if (ssum < dp[n][i])ssum = dp[n][i];
+	}
+
+	cout << ssum;
+
+	return 0;
+}
