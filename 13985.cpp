d028960b1d45333e1056d993a15663e48501c62f
jinith <96716425+L-jin@users.noreply.github.com>
2022-02-02 23:44:46 +0900

    Create 13985.cpp

---
+++ 13985.cpp
@@ -0,0 +1,18 @@
+//BOJ 13985 - 22.02.02
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    int a,b,d;
+    cin>>a>>c>>b>>c>>d;
+    
+    if (a+b==d) cout<<"YES";
+    else cout<<"NO";
+
+
+
+return 0;
+}
