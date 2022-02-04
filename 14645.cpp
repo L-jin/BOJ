0cc71c484b2d0991e2ac02f06dff5e55df02bc4f
jinith <96716425+L-jin@users.noreply.github.com>
2022-02-04 16:36:07 +0900

    Create 14645.cpp

---
+++ 14645.cpp
@@ -0,0 +1,19 @@
+//BOJ 14645 - 22.02.04
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    
+    cin>>a>>b;
+    
+    int c;
+    
+    for (int i=0; i<a*2; i++)
+       cin>>c;
+    
+    cout<<"비와이";
+    
+    return 0;
+}
